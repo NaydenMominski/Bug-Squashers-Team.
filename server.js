0669b8aa74412f7cdc480114bde82491a385694b
NaydenMominski <mominski@abv.bg>
2017-07-16 21:09:49 +0300

    make  mlab conection

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const { getApp } = require('./app/app');
 
 const port = 3005;
+// ----mlab mongodb------
+// getApp({ connectionString: 'mongodb://nodeproject:project@ds161262.mlab.com:61262/nodeproject' })
 
 getApp({ connectionString: 'mongodb://localhost/PropertyDb' })
     .then((app) =>
