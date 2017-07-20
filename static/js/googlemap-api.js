27873f35b8da7303f68658ea6d5cb11e4f747b69
Doroteya Nikolova <doroteanikolova@gmail.com>
2017-07-20 17:23:59 +0300

    save coords in db

--- static/js/googlemap-api.js
+++ static/js/googlemap-api.js
@@ -10,7 +10,7 @@ function initMap() {
     const map = new google.maps.Map(document.getElementById('map'), options);
 
     // Add marker
-    addMarker({
+    addMarker(map, {
         coords: { lat: 42.698334, lng: 42.698334 },
         content: '<h3>Current position</h3>'
     });
@@ -24,8 +24,9 @@ function initMap() {
         let i, place;
 
         for (i = 0; place = places[i]; i++) {
+
             bounds.extend(place.geometry.location);
-            addMarker({
+            addMarker(map, {
                 coords: place.geometry.location,
                 content: `<div><strong>${place.name}</strong><br>
                 ${place.formatted_address}</div>`
@@ -37,7 +38,7 @@ function initMap() {
     });
 
     // add marker
-    function addMarker(props) {
+    function addMarker(map, props) {
         const marker = new google.maps.Marker({
             position: props.coords,
             map: map,
@@ -46,8 +47,16 @@ function initMap() {
 
         const infoWindow = new google.maps.InfoWindow({ content: props.content });
 
+        document.getElementById('long-map').value = marker.getPosition().lng();
+        document.getElementById('lat-map').value = marker.getPosition().lat();
+
         marker.addListener('click', () => {
             infoWindow.open(map, marker);
         });
     }
+
+    $('a[href="#tab-location"]').on('shown.bs.tab', function (e) {
+        var target = $(e.target).attr("href") // activated tab
+        alert(target);
+    });
 }
