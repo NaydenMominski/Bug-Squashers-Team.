64ae7cde2f5aa8425d08b7dbb2f56a9febace941
Doroteya Nikolova <doroteanikolova93@gmail.com>
2017-07-24 19:49:52 +0300

    Add static sell detail map

--- static/js/googlemap-api.js
+++ static/js/googlemap-api.js
@@ -8,11 +8,13 @@ jQuery(function($) {
             center: new google.maps.LatLng(42.698334, 42.698334)
         }
 
+        let marker;
+
         const drawingManager = new google.maps.drawing.DrawingManager();
         const map = new google.maps.Map($map[0], options);
         drawingManager.setMap(map);
 
-
+        // add place
         if ($map[0].id === 'map-location') {
             const searchBox = new google.maps.places.SearchBox(document.getElementById('address'));
             google.maps.event.addListener(searchBox, 'places_changed', () => {
@@ -29,60 +31,56 @@ jQuery(function($) {
                         content: `<div><strong>${place.name}</strong><br>
                 ${place.formatted_address}</div>`
                     });
-                    document.getElementById('long-map').value = place.geometry.location;
+                    document.getElementById('lat').value = place.geometry.location.lat();
+                    document.getElementById('lng').value = place.geometry.location.lng();
                 }
 
                 map.fitBounds(bounds);
                 map.setZoom(15);
 
             });
-        }
+            const geocoder = new google.maps.Geocoder();
+            google.maps.event.addListener(map, 'click', function(e) {
+                const latLng = e.latLng;
+                geocoder.geocode({
+                    'latLng': latLng
+                }, (results, status) => {
+                    if (status == google.maps.GeocoderStatus.OK) {
+                        if (results[0]) {
+                            console.log(results[0]);
+                            addMarker(map, {
+                                    coords: latLng,
+                                    content: `<div><strong>${results[0].formatted_address}</strong></div>`
+                                }
 
-        if (coords) {
-            console.log(coords)
-            addMarker(map, {
-                coords: coords
+                            );
+                        }
+                    }
+                });
+
             });
         }
-
         // add marker
         function addMarker(map, props) {
-            const marker = new google.maps.Marker({
-                position: props.coords,
-                map: map,
-                draggable: true
-            });
+            if (!marker) {
+                marker = new google.maps.Marker({
+                    position: props.coords,
+                    icon: '/static/pictures/house-02.png',
+                    map: map,
+                    draggable: true
+                });
 
-            const infoWindow = new google.maps.InfoWindow({ content: props.content });
-            marker.addListener('click', () => {
-                infoWindow.open(map, marker);
-            });
+                const infoWindow = new google.maps.InfoWindow({ content: props.content });
+                marker.addListener('click', () => {
+                    infoWindow.open(map, marker);
+                });
+            } else {
+                marker.setPosition(props.coords);
+            }
         }
 
     }
 
-
-    $('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
-        const target = $(e.target).attr("href");
-
-
-
-        if ((target == '#tab-location')) {
-            mapIndex++;
-            let coords = $(this).data('coords');
-            let locationToStringed = coords;
-            let input = locationToStringed.replace('(', '');
-            let latlngStr = input.split(",", 2);
-            let lat = parseFloat(latlngStr[0]);
-            let lng = parseFloat(latlngStr[1]);
-            let parsedPosition = new google.maps.LatLng(lat, lng);
-
-
-            $('.maps').html('').append('<div class="map-' + mapIndex + '" style="width: 800px; height: 500px;"></div>');
-            initMap($('.map-' + mapIndex), parsedPosition);
-        }
-
-    });
-
     initMap();
+
 });
