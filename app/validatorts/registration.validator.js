b20b86e2762b302966f4790424b369576f1a957a
arushev <deso91@abv.bg>
2017-07-26 11:25:53 +0300

    added info

--- app/validatorts/registration.validator.js
+++ app/validatorts/registration.validator.js
@@ -1,8 +1,24 @@
 const getController = (data) => {
     return {
         check(user) {
-            user.checkBody('username', 'Name is required').notEmpty();
-            user.checkBody('password', 'Password is required').notEmpty();
+            user.checkBody({
+                'username': {
+                    notEmpty: true,
+                    matches: {
+                        options: [/^[a-zA-Z0-9]([._](?![._])|[a-zA-Z0-9]){1,18}[a-zA-Z0-9]$/],
+                    },
+                    errorMessage: 'Invalid Username', // Error message for the parameter
+                },
+                'password': {
+                    notEmpty: true,
+                    matches: {
+                        options: [/^[0-9a-zA-Z]{5,30}$/],
+                    },
+                    errorMessage: 'Invalid Password', // Error message for the parameter
+                },
+            });
+
+            user.checkBody('password2', 'Passwords are not the same').equals(user.body.password);
 
             const errors = user.validationErrors();
 
