c55448e1ca7361ac99dc53c910a72fb17579b15c
NaydenMominski <mominski@abv.bg>
2017-07-28 01:34:49 +0300

    return last fix

--- app/routers/user/controller.js
+++ app/routers/user/controller.js
@@ -3,19 +3,19 @@
 const getController = (data) => {
     return {
         getUserAllProperty(req, res) {
-            // return res.json(req.user);
-            return Promise.all([data.getUserAllRents(req.user.username),
-                    data.getUserAllSells(req.user.username),
-                ])
-                .then(([rents, sells]) => {
-                    return res.render('user/dashboard', {
-                        sells: sells,
-                        rents: rents,
-                    });
-                })
-                .catch((err) => {
-                    return res.redirect(404, '/sells');
-                });
+            return res.json(req.user);
+            // return Promise.all([data.getUserAllRents(req.user.username),
+            //         data.getUserAllSells(req.user.username),
+            //     ])
+            //     .then(([rents, sells]) => {
+            //         return res.json(req.user).render('user/dashboard', {
+            //             sells: sells,
+            //             rents: rents,
+            //         });
+            //     })
+            //     .catch((err) => {
+            //         return res.redirect(404, '/sells');
+            //     });
         },
     };
 };
